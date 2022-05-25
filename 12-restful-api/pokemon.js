cbc59dbce354adaf6b8e603d69086d5105f92469
kunxin-chor <chorkunxin@yahoo.com>
2022-05-25 13:15:14 +0000

    Added pokemon search example

---
+++ 12-restful-api/pokemon.js
@@ -0,0 +1,23 @@
+const BASE_API_URL = "https://pokeapi.co/api/v2";
+
+document.querySelector("#btnSearch")
+    .addEventListener('click', async function () {
+        let pokemonToSearchFor = document.querySelector('#txtSearch').value;
+        let endpointURL = BASE_API_URL + '/pokemon/' + pokemonToSearchFor.toLowerCase();
+        try {
+
+            let response = await axios.get(endpointURL);
+
+            let resultDiv = document.querySelector('#results');
+            resultDiv.innerHTML = `
+            <img src="${response.data.sprites.front_shiny}"/>
+            <ul>
+                <li>Base Experience: ${response.data.base_experience}</li>
+                <li>Weight: ${response.data.weight}</li>            
+            </ul>`
+        } catch (e) {
+            alert("Sorry! Pokemon is not found!");
+        }
+
+
+    })
